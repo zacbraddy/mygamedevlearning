4259fb61aa5f9527e86faf8053e769167e8f8bb7
Zac Braddy <zacharybraddy@gmail.com>
2020-04-27 09:04:06 +0100

    Started putting together reviews in the podcasts page for episode by episode reviews

---
+++ src/components/molecules/review-container.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+export default ({ children }) => {
+  const [showingReviews, setShowingReviews] = useState(false)
+  return (
+    <div className="border-t-2 border-secondary-400">
+      <div className="text-lg font-bold text-center text-primary-400">
+        Episode Reviews
+      </div>
+      <div className="flex justify-center my-8">
+        <button
+          onClick={() => setShowingReviews(!showingReviews)}
+          className="px-4 py-2 font-bold border-4 rounded border-secondary-400 bg-tertiary-400 text-secondary-400"
+        >
+          {showingReviews ? 'Hide' : 'Show'}
+        </button>
+      </div>
+      <div className={`${showingReviews ? '' : 'hidden'}`}>{children}</div>
+    </div>
+  )
+}
--- src/components/organisms/ReviewItem.js
+++ src/components/organisms/ReviewItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ImageHeaderCard from './ImageHeaderCard'
 import Tag from '../atoms/tag'
+import ReviewContainer from '../molecules/review-container'
 
 export default ({ headerSrc, headerAlt, headerText, content, tags }) => (
   <ImageHeaderCard
@@ -9,6 +10,7 @@ export default ({ headerSrc, headerAlt, headerText, content, tags }) => (
     headerText={headerText}
   >
     <div className="p-4">{content}</div>
+    <ReviewContainer>Reviews</ReviewContainer>
     <div className="flex flex-wrap p-4 border-t-2 border-secondary-400">
       {tags.map((tag, idx) => (
         <Tag>{tag}</Tag>
