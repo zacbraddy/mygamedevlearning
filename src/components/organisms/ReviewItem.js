d9a21dcd322f2de33a4104298e62d916b3aff263
Zac Braddy <zacharybraddy@gmail.com>
2020-04-27 09:06:19 +0100

    Only show episode reviews if there are some

--- src/components/organisms/ReviewItem.js
+++ src/components/organisms/ReviewItem.js
@@ -3,14 +3,21 @@ import ImageHeaderCard from './ImageHeaderCard'
 import Tag from '../atoms/tag'
 import ReviewContainer from '../molecules/review-container'
 
-export default ({ headerSrc, headerAlt, headerText, content, tags }) => (
+export default ({
+  headerSrc,
+  headerAlt,
+  headerText,
+  content,
+  tags,
+  episodeReview,
+}) => (
   <ImageHeaderCard
     headerSrc={headerSrc}
     headerAlt={headerAlt}
     headerText={headerText}
   >
     <div className="p-4">{content}</div>
-    <ReviewContainer>Reviews</ReviewContainer>
+    {episodeReview && <ReviewContainer>Reviews</ReviewContainer>}
     <div className="flex flex-wrap p-4 border-t-2 border-secondary-400">
       {tags.map((tag, idx) => (
         <Tag>{tag}</Tag>
